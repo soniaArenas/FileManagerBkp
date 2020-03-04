35ec93612628c0f6bf3be2ebf73765cd94844499
soniaArenas <puente46191@gmail.com>
2020-03-04 16:48:39 +0100

    XML Test

--- FileManager/FileManager.DataAccess.DataTests/XmlTest.cs
+++ FileManager/FileManager.DataAccess.DataTests/XmlTest.cs
@@ -1,20 +1,57 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using FileManager.DataAccess.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using System.Collections.Generic;
+using System.IO;
+using FileManager.DataAccess.Data.AbstractFactory;
+using FileManager.DataAccess.Data.AbstractProduct;
+using FileManager.DataAccess.Data.Product;
+using FileManager.DataAccess.Data;
+using FileManager.Common.Layer;
 
-namespace FileManager.DataAccess.Data.Tests
+namespace FileManager.DataAccess.DataTests
 {
-    [TestClass()]
-    public class StudientDaoTests
+    /// <summary>
+    /// Descripción resumida de XmlTest
+    /// </summary>
+    [TestClass]
+    public class XmlTest
     {
-        [TestMethod()]
-        public void AddTest()
+
+        [ClassInitialize]
+        public static void TestFixtureSetup(TestContext context)
         {
-            Assert.Fail();
+            string path = ConfigurationManager.AppSettings["xml"].ToString();
+            XmlUtility.Serialize();
         }
+        [TestInitialize]
+        public void Setup()
+        {
+            Xml NewXml = new Xml();
+            Student student1 = new Student("1", "Pepe", "Pepe", "45");
+            Student student2 = new Student("2", "Laura", "Laura", "20");
+            Student student3 = new Student("3", "Cris", "Cris", "12");
+            Student student4 = new Student("4", "Lolo", "Lolo", "15");
+            NewXml.Create(student1);
+            NewXml.Create(student2);
+            NewXml.Create(student3);
+            NewXml.Create(student4);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            string path = ConfigurationManager.AppSettings["xml"].ToString();
+            XmlUtility.Serialize();
+        }
+        /*  [ClassCleanup]
+          public static void TestFixtureTearDown()
+          {
+              string path = ConfigurationManager.AppSettings["xml"].ToString();
+              File.Delete(path);
+          }*/
+
+
     }
 }
