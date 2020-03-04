fcabf0e51df0e89e1a68e6f9ff75da1039842ce3
soniaArenas <puente46191@gmail.com>
2020-03-04 16:41:43 +0100

    commit

---
+++ FileManager/FileManager.DataAccess.Data/Product/Json.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileManager.DataAccess.Data.AbstractProduct;
+
+namespace FileManager.DataAccess.Data.Product
+{
+    class Json : IFile
+    {
+        public override void CreateFile(object student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeleteFile(object student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReadFile(object student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateFile(object student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FileManager/FileManager.DataAccess.DataTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileManager.DataAccess.DataTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
