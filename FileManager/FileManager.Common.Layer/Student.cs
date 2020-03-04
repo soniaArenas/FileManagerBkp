457559ed59734e474e30964fdde8b4ba10c358f0
soniaArenas <puente46191@gmail.com>
2020-03-04 16:52:47 +0100

    Add Constructor without params.

--- FileManager/FileManager.Common.Layer/Student.cs
+++ FileManager/FileManager.Common.Layer/Student.cs
@@ -3,23 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace FileManager.Common.Layer
 {
     public class Student
     {
-       public int StudentId { get; set; }
+        [XmlElement("ID")]
+        public string StudentId { get; set; }
+        [XmlElement("Name")]
         public string StudentName { get; set; }
+        [XmlElement("Surname")]
         public string StudentSurname { get; set; }
+        [XmlElement("Age of birth")]
         public string studentAge { get; set; }
-       
 
-        public Student (int Id,string name, string surname, string age)
+        public static List<Student> students = new List<Student>();
+        public Student (string Id,string name, string surname, string age)
         {
-            StudentId = Id;
+            
+        StudentId = Id;
             StudentName = name;
             StudentSurname = surname;
             studentAge = age;
+        }
+        public Student()
+        {
+
+            StudentId = StudentId;
+            StudentName = StudentName;
+            StudentSurname = StudentSurname;
+            studentAge = studentAge;
         }
 
     }
