97796c6d5d456a06596e56c63a789271fca4e7e5
soniaArenas <puente46191@gmail.com>
2020-03-05 21:23:21 +0100

    Business Layer finished

--- FileManager/FileManager.Business.Layer/StudentBusiness.cs
+++ FileManager/FileManager.Business.Layer/StudentBusiness.cs
@@ -1,42 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FileManager.Common.Layer;
 using FileManager.DataAccess.Data;
 using FileManager.DataAccess.Data.AbstractProduct;
 
 namespace FileManager.Business.Layer
 {
-    class StudentBusiness
+    public class StudentBusiness
     {
-        public void Add(string Name, string Surname, DateTime AgeOfBirth, string type)
+        public static void Add(Student student, string TypeFile)
         {
-            Student student = new Student();
-            student.StudentName = Name;
-            student.StudentSurname = Surname;
-            student.AgeOfBirth = AgeOfBirth;
-            IFile studenCreator = FactoryProvider.GetFactory(type).CreateFile(type);
+            IFile studenCreator = FactoryProvider.GetFactory(TypeFile).CreateFile(TypeFile);
             studenCreator.Create(student);
         }
-        public void Delete(string Name, string Surname, DateTime AgeOfBirth, string StudentId, string TypeFile)
+        public void Delete(Student student, string TypeFile)
         {
-            Student student = new Student();
-            student.StudentName = Name;
-            student.StudentSurname = Surname;
-            student.AgeOfBirth = AgeOfBirth;
-            student.StudentId = Int32.Parse(StudentId);
+            
             IFile studenCreator = FactoryProvider.GetFactory(TypeFile).CreateFile(TypeFile);
             studenCreator.Delete(student);
         }
-        public void Update(string Name, string Surname, DateTime AgeOfBirth, int StudentId, string TypeFile)
+        public void Update(Student student, string TypeFile)
         {
-            Student student = new Student();
-            student.StudentName = Name;
-            student.StudentSurname = Surname;
-            student.AgeOfBirth = AgeOfBirth;
-            student.StudentId = StudentId;
+           
             IFile studenCreator = FactoryProvider.GetFactory(TypeFile).CreateFile(TypeFile);
             studenCreator.Update(student);
         }
