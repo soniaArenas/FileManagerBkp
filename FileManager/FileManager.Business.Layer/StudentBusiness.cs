d6b70de387a50031f9e186ec30afd383a46663f4
soniaArenas <puente46191@gmail.com>
2020-03-05 20:54:24 +0100

    Business.Layer

---
+++ FileManager/FileManager.Business.Layer/StudentBusiness.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileManager.Common.Layer;
+using FileManager.DataAccess.Data;
+using FileManager.DataAccess.Data.AbstractProduct;
+
+namespace FileManager.Business.Layer
+{
+    class StudentBusiness
+    {
+        public void Add(string Name, string Surname, DateTime AgeOfBirth, string type)
+        {
+            Student student = new Student();
+            student.Name = Name;
+            student.Surname = Surname;
+            student.AgeOfBirth = AgeOfBirth.ToString();
+            IFile studenCreator = FactoryProvider.GetFactory(type).Creator(type);
+            studenCreator.Add(student);
+        }
+        public void Delete(string Name, string Surname, DateTime AgeOfBirth, string StudentId, string typo)
+        {
+            Student student = new Student();
+            student.Name = Name;
+            student.Surname = Surname;
+            student.AgeOfBirth = AgeOfBirth.ToString();
+            student.StudentId = Int32.Parse(StudentId);
+            IFile studenCreator = FactoryProvider.GetFactory(type).Creator(type);
+            studenCreator.Delete(student);
+        }
+        public void Update(string Name, string Surname, DateTime AgeOfBirth, string StudentId, string typo)
+        {
+            Student student = new Student();
+            student.Name = Name;
+            student.Surname = Surname;
+            student.AgeOfBirth = AgeOfBirth.ToString();
+            student.StudentId = Int32.Parse(StudentId);
+            StudentDao studenCreator = FactoryProvider.GetFactory(typo).Creator(typo);
+            studenCreator.Update(student);
+        }
+    }
+}
